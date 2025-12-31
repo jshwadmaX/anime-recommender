00f1be39b0ab4cd44495f6e36ed4ea1f2bd5a512
Arya Shinde <aryabshinde@gmail.com>
2025-12-31 17:50:01 +0530

    Reorder imports in app.py for clarity

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from model import recommend
-import os
+
 
 app = Flask(__name__)
 
@@ -17,10 +17,11 @@ def rec():
     return render_template("result.html", anime=anime, results=results)
 
 
-
+import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
